510040874cfc354d29fcbecf36e0f21525c69a00
Scott <scott.ph.chou@gmail.com>
2022-03-04 17:40:26 +0800

    Set appId mandatory

--- Sources/Core/BloctoSDK.swift
+++ Sources/Core/BloctoSDK.swift
@@ -9,15 +9,10 @@ import Foundation
 
 final public class BloctoSDK {
 
-    /// For now, it's optional.
     private let appId: String?
-    private let appName: String
-    private let appImageURLString: String?
 
-    init(appId: String?, appName: String, appImageURLString: String?) {
+    init(appId: String) {
         self.appId = appId
-        self.appName = appName
-        self.appImageURLString = appImageURLString
     }
 
     func requestAccount(blockchain: Blockchain, completion: (Result<String, BlockSDKError>) -> Void) {
