f1eba01c3cc63b8d8c24df9ac141ac0b028d1418
andrew54068 <andrew0424718012@yahoo.com.tw>
2022-04-26 13:08:25 +0800

    [refine] adjust swift flag.

--- Sources/Core/Extensions/BundleExtension.swift
+++ Sources/Core/Extensions/BundleExtension.swift
@@ -10,10 +10,12 @@ import Foundation
 extension Bundle {
 
     static var resouceBundle: Bundle? {
-#if SWIFT_PACKAGE
+#if COCOAPODS
+        return Bundle(identifier: "org.cocoapods.BloctoSDK")
+#elseif SWIFT_PACKAGE
         return Bundle.resouceBundle
 #else
-        return Bundle(identifier: "org.cocoapods.BloctoSDK")
+        return nil
 #endif
     }
 
