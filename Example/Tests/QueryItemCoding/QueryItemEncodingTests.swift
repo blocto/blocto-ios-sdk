8962cd007d003db20e3a3e92635a5827a4785f43
andrew54068 <andrew0424718012@yahoo.com.tw>
2022-05-13 22:19:12 +0800

    [feat] add evm base query test.

--- Example/Tests/QueryItemCoding/QueryItemEncodingTests.swift
+++ Example/Tests/QueryItemCoding/QueryItemEncodingTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 @testable import BloctoSDK
+import BigInt
 
 class QueryItemEncodingTests: XCTestCase {
 
@@ -93,6 +94,44 @@ class QueryItemEncodingTests: XCTestCase {
 
         // Then:
         XCTAssertDictionaryEqual(dictionary, expect)
+    }
+
+    func testEVMBaseTransactionValueEncoding() throws {
+        // Given:
+        let requestId: String = "FB09703B-7D29-44C6-BEB6-7C0FBEA0D585"
+        let blockchain: Blockchain = .ethereum
+        let to = "0x58F385777aa6699b81f741Dd0d5B272A34C1c774"
+        let from = "0xC823994cDDdaE5cb4bD1ADFe5AfD03f8E06Bc7ef"
+        let value = BigUInt(256)
+        let dataString = "5524107700000000000000000000000000000000000000000000000000000000000015be"
+        let evmBaseTransaction: EVMBaseTransaction = .init(
+            to: to,
+            from: from,
+            value: value,
+            data: dataString.hexDecodedData)
+        let methodContentType: MethodContentType = .sendTransaction(transaction: evmBaseTransaction)
+
+        let expect: [URLQueryItem] = [
+            URLQueryItem(name: QueryName.appId.rawValue, value: appId),
+            URLQueryItem(name: QueryName.requestId.rawValue, value: requestId),
+            URLQueryItem(name: QueryName.blockchain.rawValue, value: blockchain.rawValue),
+            URLQueryItem(name: QueryName.method.rawValue, value: MethodType.sendTransaction.rawValue),
+            URLQueryItem(name: QueryName.from.rawValue, value: from),
+            URLQueryItem(name: QueryName.to.rawValue, value: to),
+            URLQueryItem(name: QueryName.value.rawValue, value: "100"),
+            URLQueryItem(name: QueryName.data.rawValue, value: dataString)
+        ]
+
+        // When:
+        let queryItems = URLEncoding.queryItems(
+            appId: appId,
+            requestId: requestId,
+            blockchain: blockchain,
+            method: methodContentType)
+        let queryComponents = URLEncoding.encode(queryItems)
+
+        // Then:
+        XCTAssertArrayElementsEqual(queryComponents, expect)
     }
 
 }
