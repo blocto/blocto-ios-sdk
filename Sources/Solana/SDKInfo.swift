80ea9213490e8bf9599d02f10f54fe032775b2de
andrew54068 <andrew0424718012@yahoo.com.tw>
2022-09-16 12:49:50 +0800

    chore: bump version.

--- Sources/Solana/SDKInfo.swift
+++ Sources/Solana/SDKInfo.swift
@@ -13,7 +13,7 @@ enum SDKInfo {
         #if COCOAPODS
         return Bundle.resouceBundle?.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "unknown"
         #else
-        return "0.3.6"
+        return "0.3.7"
         #endif
     }
 
