6c24b03c33076f9d5f17c013483dad040a687467
Scott <scott.ph.chou@gmail.com>
2022-10-17 23:40:42 +0800

    Bump version

--- Sources/Solana/SDKInfo.swift
+++ Sources/Solana/SDKInfo.swift
@@ -13,7 +13,7 @@ enum SDKInfo {
         #if COCOAPODS
         return Bundle.resouceBundle?.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "unknown"
         #else
-        return "0.4.1"
+        return "0.4.2"
         #endif
     }
 
